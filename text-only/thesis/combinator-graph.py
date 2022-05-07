296f431613003a353589a7f52d9ef340131ed7bb
Conor Hoekstra <codereport@outlook.com>
2022-05-07 12:43:53 -0400

    Changes

--- text-only/thesis/combinator-graph.py
+++ text-only/thesis/combinator-graph.py
@@ -1,29 +1,54 @@
 import graphviz as gv
 
-combinators = { 
-    # 'ε' : ['Ê'],
-    # 'E' : ['Ê'],
-    #'D₂' : ['Ê'],
-    'D' : ['D₂',], #'E'],
-    # '∆' : ['D₂',], # 'ε'],
-    'Φ' : ['D₂',], #'E\''],
-    'S' : ['Φ', 'D'],
-    # 'Σ' : ['∆' ,'S\''],
-    'Ψ' : ['D₂'],
-    'W' : ['S','Ψ'], # 'Σ'
-    # 'E\'' : ['Ê'],
+EQ = 0 # EQUALITY
+ID = 1 # IDENTITY
+BOTH = 2
+
+e_combinators = { 
+    'ε' : ['Ê'],
+    'E' : ['Ê'],
+    'Φ₁' : ['Ê'],
 }
 
+d2_combinators = { 
+    #'D₂' : ['Ê'],
+    'D'  : [('D₂', ID)], #'E'],
+    '∆' : [('D₂', ID)], # 'ε'],
+    'Σ' : [('∆', EQ), ('Φ', ID), ('Ψ', ID)], # 'ε'],
+    'Φ'  : [('D₂', EQ)], #'E\''],
+    'S'  : [('Φ', ID), ('D', EQ), ('Ψ', ID)],
+    'Ψ'  : [('D₂', EQ)],
+    'W'  : [('S',ID) , ('Σ', ID)], # 'Σ'
+}
+
+d2bqn_combinators = { 
+    '⟜₂'  : [('⊸⟜', ID)], #'E'],
+    '⊸₂' : [('⊸⟜', ID)], # 'ε'],
+    '⊸₁' : [('⊸₂', EQ), ('3T', ID), ('○', ID)], # 'ε'],
+    '3T'  : [('⊸⟜', EQ)], #'E\''],
+    '⟜₁'  : [('3T', ID), ('⟜₂', EQ), ('○', ID)],
+    '○'  : [('⊸⟜', EQ)],
+    '˜'  : [('⟜₁',ID) , ('⊸₁', ID)], # 'Σ'
+}
+
+combinators = d2_combinators
+
 dot = gv.Digraph('combinator-graph', format = 'png')
+dot.attr(size='7,7!')
 
 for combinator in combinators.keys():
-    # if combinator in monadic:
-        # dot.node(combinator, style='filled', fillcolor = 'gray')
-    # else: 
-    dot.node(combinator)
+    l = combinator.replace('₁', '')
+    l = l.replace('₂', '')
+    if combinator in ['S', 'W', 'Φ', 'Σ', '3T', '˜', '⊸₁', '⟜₁']:
+        dot.node(combinator, style='filled', fillcolor = 'gray', label=l)
+    else: 
+        dot.node(combinator, label=l)
 
 for combinator, deps in combinators.items():
-    for dep in deps:
-        dot.edge(dep, combinator)
+    for (dep, t) in deps:
+        if t == EQ:   s = 'dashed'
+        if t == ID:   s = 'solid'
+        if t == BOTH: s = 'dashed,bold'
+        dot.edge(dep, combinator, style=s)
 
 dot.render(directory='.').replace('\\', '/')
